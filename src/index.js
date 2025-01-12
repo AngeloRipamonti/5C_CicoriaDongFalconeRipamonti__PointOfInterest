18566f219c81e6c558812f113e95dd2adbe51587
Andrea Falcone <157415531+AndreaFalcone1@users.noreply.github.com>
2025-01-12 16:58:29 +0100

    Completatate le modali

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+//POI
 document.getElementById("modalInsertAdminButton").onclick = () => {
-    document.getElementById("authentication-modal").classList.remove("hidden");
+    document.getElementById("authentication-modal-POI").classList.remove("hidden");
+}
+document.getElementById("close-modal-POI").onclick = () => {
+    document.getElementById("authentication-modal-POI").classList.toggle("hidden");
+}
+
+//Login
+document.getElementById("modalAdminLogin").onclick = () => {
+    document.getElementById("authentication-modal-Login").classList.remove("hidden");
+}
+document.getElementById("close-modal-Login").onclick = () => {
+    document.getElementById("authentication-modal-Login").classList.toggle("hidden");
 }
